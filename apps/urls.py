1c76c9f45875a643001d6a2ea1dea899a2e5d2a1
Jav0x1r01 <javoxirmamatayev@gmail.com>
2024-01-17 00:36:21 +0500

    exsam

--- apps/urls.py
+++ apps/urls.py
@@ -4,14 +4,14 @@ from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
 
-from apps.views import UserModelView, delete_product, add_product
+from apps.views import UserModelView, delete_product, AddProductView
 
 urlpatterns = [
     path('', UserModelView.as_view(), name='index'),
-    path('add_product',add_product,name='add_product')
+    path('add_product',AddProductView.as_view(),name='add_product'),
+    path('del/<int:pk>',delete_product,name='delete_product'),
+
+
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- apps/views.py
+++ apps/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView,TemplateView
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import ListView, TemplateView, FormView
 
+from apps.forms import ProductForm
 from apps.models import UserModel, Position
 
 
 class UserModelView(ListView):
     template_name = 'apps/index.html'
-    queryset=UserModel.objects.all()
+    queryset = UserModel.objects.all()
     context_object_name = 'users'
 
 
@@ -15,12 +18,30 @@ def delete_product(request, pk):
     return redirect('index')
 
 
-def add_product(request):
-    context=None
-    if request.method == 'POST':
-        context = {
-            'positions': Position.objects.all()
-        }
-        return redirect('products_list')
-    return render(request, 'apps/forms.html', context)
+# class AddProductView(View):
+#     def get(self, request):
+#         context = {
+#             'positions': Position.objects.all()
+#         }
+#         return render(request, 'apps/forms.html', context)
+#
+#     def post(self, request):
+#         # Post so'rovi bo'yicha kerakli logika
+#         return redirect('index')
 
+
+class AddProductView(FormView):
+    template_name = 'apps/forms.html'
+
+    # context_object_name='positions'
+    form_class = ProductForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['positions'] = Position.objects.all()
+        return context
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
